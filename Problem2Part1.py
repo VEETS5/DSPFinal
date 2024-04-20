e00abce2f8c101d6e9d99d6b85fea797ac904965
VEETS5 <torinavito5@gmail.com>
2024-04-20 13:52:33 -0400

    finished p1p1

--- Problem2Part1.py
+++ Problem2Part1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-poles = [0, 0]  
+poles = [0.9*(np.exp(1j * np.pi/3)), 0.9*(np.exp(-1j * np.pi/3))]  
 zeros = [np.exp(1j * np.pi/3), np.exp(-1j * np.pi/3)]
 
 fig, ax = plt.subplots(figsize=(6, 6))
@@ -9,8 +9,8 @@ fig, ax = plt.subplots(figsize=(6, 6))
 unit_circle = plt.Circle((0, 0), 1, fill=False, linestyle='--', color='gray')
 ax.add_artist(unit_circle)
 
-ax.scatter(np.real(poles), np.imag(poles), marker='o', facecolors='none', edgecolors='r', s=50, label='Poles')
-ax.scatter(np.real(zeros), np.imag(zeros), marker='x', color='b', s=50, label='Zeros')
+ax.scatter(np.real(poles), np.imag(poles), marker='x', color='r', s=50, label='Poles')
+ax.scatter(np.real(zeros), np.imag(zeros), marker='o', color='b', s=50, label='Zeros')
 
 ax.set_xlabel('Real Part')
 ax.set_ylabel('Imaginary Part')
